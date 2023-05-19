a65b0ad4c3c561850592143ff663ce305924d011
ashleytrollinger <ashley@trollingers.com>
2023-05-19 17:28:11 -0400

    helper folder added and index.js in routes is done

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+const indexRouter = require('./index');
+
+
+const app = express();
+
+app.use('/notes', notesRouter);
+app.use('/index', indexRouter);
+
+module.exports = app;
