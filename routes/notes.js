546e7ddf2b5da77d014d47bc26042e60f3af0704
ashleytrollinger <ashley@trollingers.com>
2023-05-19 17:35:19 -0400

    Universal Identifier npm added

---
+++ routes/notes.js
@@ -0,0 +1,25 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific tip
+notes.get('/:tip_id', (req, res) => {
+    const tipId = req.params.tip_id;
+    readFromFile('./db/tips.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((tip) => tip.tip_id === tipId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No tip with that ID');
+      });
+  });
